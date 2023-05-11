826f568766fb70cdd95796f511202da8d299741c
Yaroslav Tykhonenko <129007570+yarolavtykhonenko22@users.noreply.github.com>
2023-05-11 16:39:24 +0300

    redone work

--- Minimalnoe_chislo.py
+++ Minimalnoe_chislo.py
@@ -1,7 +1,19 @@
-def three_intenger_numbers(a, b, h):
-    print(min(a, b, h))
-    return
+def get_smallest_number(first_number, second_number, third_number):
+    if second_number >= first_number <= third_number:
+        return first_number
+    elif first_number >= second_number <= third_number:
+        return second_number
+    else:
+        return third_number
+
 first_number = int(input())
 second_number = int(input())
 third_number = int(input())
-three_intenger_numbers(first_number, second_number, third_number)
+if __name__ == "__main__":
+    get_smallest_number(first_number, second_number, third_number)
+def test():
+    test_cases = [(23, 3, 5, 3), (10, 10, 10, 10), (23, 4, 23, 4), (-1, -3, -4, -4)]
+    for *values, returns in test_cases:
+        assert get_smallest_number(*values) == returns, f"case: {values} doesn't work, expected {returns} got {get_smallest_number(*values)}"
+if __name__ == "__main__":
+    test()
--- same_numbers.py
+++ same_numbers.py
@@ -1,11 +1,20 @@
-def same_numbers(a, b, c):
-    if a / b != 1 and a / c != 1 :
-        print(0)
-    elif a / b == 1 and a / c == 1 :
-        print(3)
+def check_quantity_of_same_numbers(first_integer, second_integer, third_integer):
+    if first_integer / second_integer != 1 and first_integer / third_integer != 1:
+        return 0
+    elif first_integer / second_integer == 1 and first_integer / third_integer == 1:
+        return 3
     else:
-        print(2)
-first_integer=int(input())
-second_integer=int(input())
-third_integer=int(input())
-same_numbers(first_integer, second_integer, third_integer)
+        return 2
+first_integer = int(input())
+second_integer = int(input())
+third_integer = int(input())
+
+if __name__ == "__main__":
+    check_quantity_of_same_numbers(first_integer, second_integer, third_integer)
+def test():
+    test_cases = [(23, 3, 5, 0), (10, 10, 10, 3), (23, 4, 23, 2), (-1, -3, -4, 0)]
+    for *test_values, test_returns in test_cases:
+        assert check_quantity_of_same_numbers(*test_values) == test_returns, f"case: {test_values} doesn't work, expected {test_returns} got {check_quantity_of_same_numbers(*test_values)}"
+if __name__ == "__main__":
+    test()
+
