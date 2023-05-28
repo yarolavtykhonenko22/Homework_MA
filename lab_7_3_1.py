61c895583f0f471174a2a442d3caf95cf6bf76cc
Yaroslav Tykhonenko <129007570+yarolavtykhonenko22@users.noreply.github.com>
2023-05-28 18:01:14 +0300

    Add files via upload

--- lab_7_3_1.py
+++ lab_7_3_1.py
@@ -12,23 +12,31 @@ class Date:
     @staticmethod
     def from_string_static(string_date, splitter):
         day, month, year = map(int, string_date.split(splitter))
-        return [day, month, year]
+        return Date(day, month, year)
 
     @classmethod
     def from_string(cls, string_date, splitter):
         day, month, year = map(int, string_date.split(splitter))
-        return cls(day, month, year)
-def test1():
+        return Date(day, month, year)
+
+
+if is_date := Date.is_date_valid("12-2-345", "-"):
+    date1 = Date.from_string_static("12,2,345", ",")
+else:
+    raise ValueError()
+if date1:
+    date2 = Date.from_string("12-23-345", "-")
+else:
+    raise ValueError()
+print(is_date, date1.day, date1.month, date1.year)
+
+
+def test():
     test_cases = ["15-2-200", True], ["32-12-6534", False], ["1-13-1234", False], ["12-4-98777", False]
     for test_values, test_returns in test_cases:
         test = Date.is_date_valid(test_values, "-")
         assert test == test_returns, f"case: {test_values} doesn't work, expected {test_returns}, got {test}"
 
-def test2():            # 1 test for function from_string_static and from_string because they are same
-    test_cases = [("15-2-200", 15, 2, 200), ("32-12-6534", 32, 12, 6534), ("1-13-1234", 1, 13, 1234),("12-4-98777", 12, 4, 98777)]
-    for test_values, *test_returns in test_cases:
-        test = Date.from_string_static(test_values, "-")
-        assert test == test_returns, f"case:{test_values} doesn't work, expected{test_returns}, got {test}"
+
 if __name__ == "__main__":
-    test1()
-    test2()
+    test()
---
+++ lab_7_3_2..py
@@ -0,0 +1,13 @@
+import lab_7_3_1
+
+
+class UserDate(lab_7_3_1.Date):
+    date3 = None
+
+
+if date3 := lab_7_3_1.Date.from_string("12-2-345", "-"):
+    if date3:
+        date4 = lab_7_3_1.Date.from_string_static("12-2-345", "-")
+        if date4:
+            print(isinstance(date3, UserDate))
+            print(isinstance(date4, UserDate))
