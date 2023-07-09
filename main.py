504f65a332ad6deaf49ff4fa5eb530cfa4acf482
Yaroslav Tykhonenko <129007570+yarolavtykhonenko22@users.noreply.github.com>
2023-07-09 12:18:18 +0300

    Add files via upload
    telegram bot

--- main.py
+++ main.py
@@ -2,27 +2,36 @@ from bs4 import BeautifulSoup
 import requests
 from aiogram import types, Dispatcher, executor, Bot
 from config import TOKEN
+import json
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
-
-LINK_ZL = "https://www.google.com/search?q=zloty+hryvnya&client=firefox-b-d&sxsrf=AB5stBg8Zqat_HfNRFudM8nlzZEHlZ9OnA%3A1688238933415&ei=VXugZNqBGeqRrgS3wKOQCA&ved=0ahUKEwiagLCPnO7_AhXqiIsKHTfgCIIQ4dUDCA4&uact=5&oq=zloty+hryvnya&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIMCAAQDRCABBBGEIICMggIABAFEB4QDTIICAAQCBAeEA06BwgjEOoCECc6DwgAEIoFEOoCELQCEEMYAToYCC4QigUQxwEQ0QMQyAMQ6gIQtAIQQxgCOggIABCABBCxAzoFCAAQgAQ6EQguEIAEELEDEIMBEMcBENEDOg4ILhDHARCxAxDRAxCABDoLCC4QgAQQxwEQ0QM6CwgAEIAEELEDEIMBOgcIABCKBRBDOgwIABCKBRBDEEYQggI6CwguEIAEEMcBEK8BOggIABCABBDLAToKCAAQgAQQRhCCAjoGCAAQFhAeOggIABAWEB4QCjoHCAAQDRCABEoECEEYAFCkEFiCNmDRN2gBcAF4AIABtwKIAaoNkgEIMS4xMS4wLjGYAQCgAQGwARPAAQHaAQYIARABGAHaAQYIAhABGAg&sclient=gws-wiz-serp"
-LINK_BNB = "https://www.google.com/search?q=bnb+dolar&client=firefox-b-d&sxsrf=AB5stBgrwoWWKWGmGcCeQrHwHIq-9grDdA%3A1688238905073&ei=OXugZL_8A860rgSGhIKYDw&ved=0ahUKEwj__O2BnO7_AhVOmosKHQaCAPMQ4dUDCA4&uact=5&oq=bnb+dolar&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIHCAAQgAQQCjIICAAQgAQQywEyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMggIABAWEB4QCjIGCAAQFhAeMgYIABAWEB46BwgjEOoCECc6BwgjEIoFECc6BwgAEIoFEEM6EwguEIoFELEDEIMBEMcBENEDEEM6CwgAEIAEELEDEIMBOgoIABCKBRCxAxBDOg4ILhDHARCxAxDRAxCABDoFCAAQgAQ6CwgAEIoFELEDEIMBOg0IABCKBRCxAxCDARBDOggIABCABBCxAzoKCAAQgAQQChDLAUoECEEYAFCPBljzG2CIH2gBcAF4AIABgQGIAfwHkgEDMC45mAEAoAEBsAEKwAEB&sclient=gws-wiz-serp"
 LINK_ETH = "https://ru.investing.com/crypto/ethereum/eth-usd"
-LINK_EU = "https://www.google.com/search?client=firefox-b-d&q=euro+grivna"
-LINK_DOL = "https://www.google.com/search?client=firefox-b-d&q=dolar+grivna"
-LINK_BIT = "https://ru.investing.com/crypto/bitcoin/btc-usd"
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0"}
+
+
+def get_currency(have, want):
+    api_url = f'https://api.api-ninjas.com/v1/convertcurrency?want={want}&have={have}&amount=1'
+    response = requests.get(api_url, headers={'X-Api-Key': 'cpkY4VMRFV/EoGxTbCXVWg==n5UGKyteQ7A04nBi'})
+    if response.status_code == requests.codes.ok:
+        response_dict = json.loads(response.text)
+        return response_dict["new_amount"]
+    else:
+        print("Error:", response.status_code, response.text)
 
 
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
-    await message.answer("Hello! Welcome to CURRENCY ALERT BOT\nThis bot follow price of currency, and if price falls or rises, bot will send you sms, but be careful, if you already chose pair of currencies you cant change it before SMS is sent\n.To GET STARTED you need enter command for any pair of currency\nThere list of pair of currencies which bot can looking for:\n/BIT_USD\n/ETH_USD\n/USD_HRN\n/PLN_HRN\n/BNB_USD\n/EU_HRN")
+    await message.answer("Hello! Welcome to CURRENCY ALERT BOT\nThis bot follow price of currency, and if price falls or rises, bot will send you sms, but be careful, if you already chose pair of currencies you cant change it before SMS is sent\nTo GET STARTED you need enter command for any pair of currency\nThere list of pair of currencies which bot can looking for:\n/BTC_USD\n/ETH_USD\n/USD_UAH\n/PLN_UAH\n/EU_UAH")
 
 
-@dp.message_handler(commands=["BIT_USD"])
+@dp.message_handler(commands=["BTC_USD"])
 async def get_price(message: types.Message):
+    count = 0
     while True:
-        currency = float(bit.bi_get_currency_price().replace(".", "").replace(",", "."))
+        currency = float(bit.bi_get_currency_price())
+        if 1 > count:
+            await message.answer("Looking for Bitcoin price")
+            count += 1
         if currency >= bit.bi_fix_price + bit.bi_difference:
             await message.answer(f"Bitcoin has increased in price and now it's costs {str(currency)} dollars")
             while True:
@@ -50,10 +59,15 @@ async def get_price(message: types.Message):
         else:
             pass
 
+
 @dp.message_handler(commands=["ETH_USD"])
 async def get_price(message: types.Message):
+    count = 0
     while True:
         currency = float(eth.et_get_currency_price().replace(".", "").replace(",", "."))
+        if 1 > count:
+            await message.answer("looking for Ethereum price")
+            count += 1
         if currency >= eth.et_fix_price + eth.et_difference:
             await message.answer(f"Ethereum has increased in price and now it's costs {str(currency)} dollars")
             while True:
@@ -82,10 +96,14 @@ async def get_price(message: types.Message):
             pass
 
 
-@dp.message_handler(commands=["USD_HRN"])
+@dp.message_handler(commands=["USD_UAH"])
 async def get_price(message: types.Message):
+    count = 0
     while True:
-        currency = float(dol.dol_get_currency_price().replace(",", "."))
+        currency = float(dol.dol_get_currency_price())
+        if 1 > count:
+            await message.answer("Looking for Dollar price")
+            count += 1
         if currency >= dol.dol_fix_price + dol.dol_difference:
             await message.answer(f"Dollar has increased in price and now it's costs {str(currency)} hryvnias")
             while True:
@@ -114,10 +132,14 @@ async def get_price(message: types.Message):
             pass
 
 
-@dp.message_handler(commands=["EU_HRN"])
+@dp.message_handler(commands=["EU_UAH"])
 async def get_price(message: types.Message):
+    count = 0
     while True:
-        currency = float(eu.eu_get_currency_price().replace(",", "."))
+        currency = float(eu.eu_get_currency_price())
+        if 1 > count:
+            await message.answer("Looking for Euro price")
+            count += 1
         if currency >= eu.eu_fix_price + eu.eu_difference:
             await message.answer(f"Euro has increased in price and now it's costs {str(currency)} hryvnias")
             while True:
@@ -145,42 +167,14 @@ async def get_price(message: types.Message):
             pass
 
 
-@dp.message_handler(commands=["BNB_USD"])
+@dp.message_handler(commands=["PLN_UAH"])
 async def get_price(message: types.Message):
+    count = 0
     while True:
-        currency = float(bnb.bnb_get_currency_price().replace(",", "."))
-        if currency >= bnb.bnb_fix_price + bnb.bnb_difference:
-            await message.answer(f"Bnb has increased in price and now it's costs {str(currency)} dollars")
-            while True:
-                if currency >= bnb.bnb_fix_price + bnb.bnb_big_difference:
-                    await message.answer(f"Bnb has greatly increased in price and now it's costs {str(currency)} dollars")
-                    break
-                elif currency <= bnb.bnb_fix_price - bnb.bnb_big_difference:
-                    await message.answer(f"Bnb has greatly fell in price and now it's costs {str(currency)} dollars")
-                    break
-                elif currency <= bnb.bnb_fix_price - bnb.bnb_difference:
-                    await message.answer(f"Bnb has fell in price and now it's costs {str(currency)} dollars")
-                    break
-        elif currency <= bnb.bnb_fix_price - bnb.bnb_difference:
-            await message.answer(f"Bnb has fell in price and now it's costs {str(currency)} dollars")
-            while True:
-                if currency >= bnb.bnb_fix_price + bnb.bnb_big_difference:
-                    await message.answer(f"Bnb has greatly increased in price and now it's costs {str(currency)} dollars")
-                    break
-                elif currency <= bnb.bnb_fix_price - bnb.bnb_big_difference:
-                    await message.answer(f"Bnb has greatly fell in price and now it's costs {str(currency)} dollars")
-                    break
-                elif currency >= bnb.bnb_fix_price + bnb.bnb_difference:
-                    await message.answer(f"Bnb has increased in price and now it's costs {str(currency)} dollars")
-                    break
-        else:
-            pass
-
-
-@dp.message_handler(commands=["PLN_HRN"])
-async def get_price(message: types.Message):
-    while True:
-        currency = float(zl.zl_get_currency_price().replace(",", "."))
+        currency = float(zl.zl_get_currency_price())
+        if 1 > count:
+            await message.answer("Looking for Zloty price")
+            count += 1
         if currency >= zl.zl_fix_price + zl.zl_difference:
             await message.answer(f"Zloty has increased in price and now it's costs {str(currency)} hryvnias")
             while True:
@@ -209,18 +203,40 @@ async def get_price(message: types.Message):
             pass
 
 
+class Euro:
+    eu_difference = 2
+    eu_big_difference = 6
+
+    def __init__(self):
+        self.eu_fix_price = float(self.eu_get_currency_price())
+
+    def eu_get_currency_price(self):
+        currency = get_currency("EUR", "UAH")
+        return currency
+
+
 class Bitcoin:
     bi_difference = 400
     bi_big_difference = 1300
 
     def __init__(self):
-        self.bi_fix_price = float(self.bi_get_currency_price().replace(".", "").replace(",", "."))
+        self.bi_fix_price = float(self.bi_get_currency_price())
 
     def bi_get_currency_price(self):
-        all_content = requests.get(LINK_BIT, headers=HEADERS)
-        soup = BeautifulSoup(all_content.content, "html.parser")
-        self.bi_price = soup.findAll("span", {"class": "text-2xl"})
-        return self.bi_price[0].text
+        currency = get_currency("BTC", "USD")
+        return currency
+
+
+class Dollar:
+    dol_difference = 1.5
+    dol_big_difference = 5
+
+    def __init__(self):
+        self.dol_fix_price = float(self.dol_get_currency_price())
+
+    def dol_get_currency_price(self):
+        currency = get_currency("USD", "UAH")
+        return currency
 
 
 class Ethereum:
@@ -237,68 +253,21 @@ class Ethereum:
         return self.et_price[0].text
 
 
-class Dollar:
-    dol_difference = 1.5
-    dol_big_difference = 5
-
-    def __init__(self):
-        self.dol_fix_price = float(self.dol_get_currency_price().replace(",", "."))
-
-    def dol_get_currency_price(self):
-        all_content = requests.get(LINK_DOL, headers=HEADERS)
-        soup = BeautifulSoup(all_content.content, "html.parser")
-        self.dol_price = soup.findAll("span", {"class": "DFlfde SwHCTb"})
-        return self.dol_price[0].text
-
-
-class Euro:
-    eu_difference = 2
-    eu_big_difference = 6
-
-    def __init__(self):
-        self.eu_fix_price = float(self.eu_get_currency_price().replace(",", "."))
-
-    def eu_get_currency_price(self):
-        all_content = requests.get(LINK_EU, headers=HEADERS)
-        soup = BeautifulSoup(all_content.content, "html.parser")
-        self.eu_price = soup.findAll("span", {"class": "DFlfde SwHCTb"})
-        return self.eu_price[0].text
-
-
-class Bnb:
-    bnb_difference = 10
-    bnb_big_difference = 50
-
-    def __init__(self):
-        self.bnb_fix_price = float(self.bnb_get_currency_price().replace(",", "."))
-
-    def bnb_get_currency_price(self):
-        all_content = requests.get(LINK_BNB, headers=HEADERS)
-        soup = BeautifulSoup(all_content.content, "html.parser")
-        self.bnb_price = soup.findAll("span", {"class": "pclqee"})
-        return self.bnb_price[0].text
-
-
 class Zloty:
     zl_difference = 0.5
     zl_big_difference = 2
 
     def __init__(self):
-        self.zl_fix_price = float(self.zl_get_currency_price().replace(",", "."))
+        self.zl_fix_price = float(self.zl_get_currency_price())
 
     def zl_get_currency_price(self):
-        all_content = requests.get(LINK_ZL, headers=HEADERS)
-        soup = BeautifulSoup(all_content.content, "html.parser")
-        self.zl_price = soup.findAll("span", {"class": "DFlfde SwHCTb"})
-        return self.zl_price[0].text
+        currency = get_currency("PLN", "UAH")
+        return currency
 
 
 bit = Bitcoin()
-bnb = Bnb()
-dol = Dollar()
-eu = Euro()
 eth = Ethereum()
 zl = Zloty()
-
-
+eu = Euro()
+dol = Dollar()
 executor.start_polling(dp)
